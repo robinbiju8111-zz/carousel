0b75e0c2ef0df37fea9db4e2df6e99af0d09ab06
Robin Biju <robinbiju8111@gmaili.com>
2021-09-09 00:32:25 +0530

    carousel completed

---
+++ js/main.js
@@ -0,0 +1,17 @@
+ /**
+   * Intro Carousel
+   */
+  new Swiper('.intro-carousel', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
